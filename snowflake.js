10a922902db4b652524aa397950cffec59ff5f40
Michael MacLeod <michaelmmacleod@gmail.com>
2022-03-06 22:17:55 -0800

    use growth function

--- snowflake.js
+++ snowflake.js
@@ -1,5 +1,7 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var graphCanvas = document.getElementById('graph');
+var graphCtx = graphCanvas.getContext('2d');
 ctx.fillStyle = 'white';
 var oneSixthCircle = Math.PI * 2 / 6;
 var directions = [
@@ -22,29 +24,6 @@ function branchEnd(branch) {
     var x = branch.start.x + l * Math.cos(d);
     var y = branch.start.y + l * Math.sin(d);
     return { x: x, y: y };
-}
-function clamp(x, low, high) {
-    return Math.min(Math.max(x, low), high);
-}
-function test(cond, name) {
-    if (!cond) {
-        console.error("Test failure: ".concat(name));
-    }
-    else {
-        console.log("Test success: ".concat(name));
-    }
-}
-function buildGrowthFunction(growthInput) {
-    return function (t) {
-        var s = clamp(t, 0, 1) * growthInput.length;
-        var x = Math.floor(s);
-        var i = x === growthInput.length ? growthInput.length - 1 : x;
-        var signedScale = growthInput[i];
-        return {
-            scale: Math.abs(signedScale),
-            growthType: signedScale > 0.0 ? 'branching' : 'faceting'
-        };
-    };
 }
 function drawSnowflake(snowflake) {
     snowflake.faces.forEach(drawFace);
@@ -180,48 +159,77 @@ function addFaceToBranch(snowflake, branch) {
         direction: branch.direction
     });
 }
+function clamp(x, low, high) {
+    return Math.min(Math.max(x, low), high);
+}
+//function buildGrowthFunction(growthInput: NonEmptyArray<number>): GrowthFunction {
+//  return t => {
+//    let s = clamp(t, 0, 1) * growthInput.length;
+//    let x = Math.floor(s);
+//    let i = x === growthInput.length ? growthInput.length - 1 : x;
+//    let signedScale = growthInput[i];
+//    return {
+//      scale: Math.abs(signedScale),
+//      growthType: signedScale > 0.0 ? 'branching' : 'faceting',
+//    };
+//  };
+//}
+var growthInput = [-1, 1, -1];
+function interpretGrowth(time) {
+    var s = clamp(time, 0, 1) * growthInput.length;
+    var x = Math.floor(s);
+    var i = x === growthInput.length ? growthInput.length - 1 : x;
+    var signedScale = growthInput[i];
+    return {
+        scale: Math.abs(signedScale),
+        growthType: signedScale > 0.0 ? 'branching' : 'faceting'
+    };
+}
 var updateInterval = 10;
+var maxSteps = 6000;
 var snowflake = createInitialSnowflake();
 var step = 0;
-var faces = true;
 var intervalId = undefined;
+var currentGrowthType = undefined;
+function currentTime() {
+    return step / maxSteps;
+}
 function update() {
     step += 1;
-    if (faces) {
-        enlargeGrowingFaces(snowflake);
+    var growth = interpretGrowth(currentTime());
+    if (currentGrowthType === undefined) {
+        currentGrowthType = growth.growthType;
     }
-    else {
-        enlargeGrowingBranches(snowflake);
-    }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSnowflake(snowflake);
-    if (step % 700 === 0) {
-        if (faces) {
+    if (currentGrowthType !== growth.growthType) {
+        currentGrowthType = growth.growthType;
+        if (currentGrowthType === 'branching') {
             addBranchesToGrowingFaces(snowflake);
         }
         else {
             addFacesToGrowingBranches(snowflake);
         }
-        faces = !faces;
     }
-    if (step === 6000) {
+    if (currentGrowthType === 'branching') {
+        enlargeGrowingBranches(snowflake);
+    }
+    else {
+        enlargeGrowingFaces(snowflake);
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnowflake(snowflake);
+    if (step === maxSteps) {
         window.clearInterval(intervalId);
         console.log('done growing');
     }
 }
 intervalId = window.setInterval(update, updateInterval);
-function testBuildGrowthFunction() {
-    test(buildGrowthFunction([-1, 0.5, 0])(0.0).scale === 1, 'buildGrowthFunction1');
-    test(buildGrowthFunction([-1, 0.5, 0])(0.32).scale === 1, 'buildGrowthFunction2');
-    test(buildGrowthFunction([-1, 0.5, 0])(0.34).scale === 0.5, 'buildGrowthFunction3');
-    test(buildGrowthFunction([-1, 0.5, 0])(0.65).scale === 0.5, 'buildGrowthFunction4');
-    test(buildGrowthFunction([-1, 0.5, 0])(0.67).scale === 0.0, 'buildGrowthFunction5');
-    test(buildGrowthFunction([-1, 0.5, 0])(1).scale === 0.0, 'buildGrowthFunction6');
-    test(buildGrowthFunction([-1, 0.5, 0])(100).scale === 0.0, 'buildGrowthFunction7');
-    test(buildGrowthFunction([-1, 0.5, 0])(-100).scale === 1, 'buildGrowthFunction8');
-    test(buildGrowthFunction([-1, 0.5, 0])(0.0).growthType === 'faceting', 'buildGrowthFunction9');
-    test(buildGrowthFunction([-1, 0.5, 0])(0.32).growthType === 'faceting', 'buildGrowthFunction10');
-    test(buildGrowthFunction([-1, 0.5, 0])(0.34).growthType === 'branching', 'buildGrowthFunction11');
+function test(cond, name) {
+    if (!cond) {
+        console.error("Test failure: ".concat(name));
+    }
+    else {
+        console.log("Test success: ".concat(name));
+    }
 }
 function testBranchEnd() {
     var r1 = branchEnd({
@@ -234,166 +242,4 @@ function testBranchEnd() {
     test(Math.abs(r1.x - 1) < 0.0001, 'testBranchEnd1');
     test(Math.abs(r1.y - 0) < 0.0001, 'testBranchEnd2');
 }
-testBuildGrowthFunction();
 testBranchEnd();
-// const canvas = document.getElementById('canvas') as HTMLCanvasElement ;
-// const ctx = canvas.getContext('2d');
-// 
-// ctx.fillStyle = 'white';
-// 
-// type Angle = number;
-// type Point = { x: number, y: number };
-// 
-// const SPLIT: Angle = Math.PI * 2 / 6;
-// 
-// function point(x: number, y: number): Point {
-//   return {
-//     x: x,
-//     y: y,
-//   }
-// }
-// 
-// type Vertex = {
-//   position: Point,
-//   direction: Angle,
-//   children: Array<Vertex>,
-// };
-// 
-// function vertex(position: Point, direction: Angle): Vertex {
-//   return {
-//     position,
-//     direction,
-//     children: [],
-//   };
-// }
-// 
-// function advanceVertex(vertex: Vertex, amount: number) {
-//   let x = vertex.position.x;
-//   let y = vertex.position.y;
-// 
-//   let xp = x + Math.cos(vertex.direction) * amount;
-//   let yp = y + Math.sin(vertex.direction) * amount;
-// 
-//   let oldDistFromOrigin = Math.sqrt(x * x + y * y);
-//   let newDistFromOrigin = Math.sqrt(xp * xp + yp * yp);
-// 
-//   let diffDist = newDistFromOrigin - oldDistFromOrigin;
-// 
-//   if (diffDist > 0) {
-//     vertex.position.x = xp;
-//     vertex.position.y = yp;
-//   } else {
-//   }
-// }
-// 
-// function snowflake(): Vertex {
-//   return vertex(point(0, 0), 0);
-// }
-// 
-// function isEndpoint(v: Vertex): boolean {
-//   return v.children.length === 0;
-// }
-// 
-// function endpoints(snowflake: Vertex): Array<Vertex> {
-//   const vs = [];
-//   const todo = [snowflake];
-// 
-//   while (todo.length > 0) {
-//     const v = todo.pop();
-//     if (isEndpoint(v)) {
-//       vs.push(v);
-//     } else {
-//       v.children.forEach(c => todo.push(c));
-//     }
-//   }
-// 
-//   return vs;
-// }
-// 
-// function splitEndpoints(snowflake: Vertex) {
-//   const es = endpoints(snowflake);
-//   es.forEach(endpoint => {
-//     let direction = endpoint.direction;
-//     for (let i = 0; i < 6; i += 1) {
-//       endpoint.children.push(
-//         vertex(
-//           point(endpoint.position.x, endpoint.position.y),
-//           direction
-//         ));
-//       direction += SPLIT;
-//     }
-//   });
-// }
-// 
-// function growEndpoints(snowflake: Vertex, amount: number) {
-//   const es = endpoints(snowflake);
-//   es.forEach(endpoint => advanceVertex(endpoint, amount));
-// }
-// 
-// function toCanvasPoint(p: Point): Point {
-//   const result = point(p.x, p.y);
-// 
-//   result.x *= canvas.width;
-//   result.y *= canvas.height;
-// 
-//   result.x += canvas.width * 0.5;
-//   result.y += canvas.height * 0.5;
-// 
-//   return result;
-// }
-// 
-// function drawSnowflake(snowflake: Vertex) {
-//   const todo = [snowflake];
-//   while (todo.length > 0) {
-//     const v = todo.pop();
-//     drawVertex(v);
-//     v.children.forEach(c => todo.push(c));
-//   }
-// }
-// 
-// function drawVertex(v: Vertex) {
-//   ctx.fillStyle = 'white';
-//   ctx.strokeStyle = 'white';
-//   
-//   const p = toCanvasPoint(v.position);
-// 
-//   v.children.forEach(child => {
-//     let c = toCanvasPoint(child.position);
-// 
-//     ctx.beginPath();
-//     ctx.moveTo(p.x, p.y);
-//     ctx.lineTo(c.x, c.y);
-//     ctx.closePath();
-//     ctx.stroke();
-//   });
-// }
-// 
-// ctx.globalAlpha = 0.15;
-// 
-// const splitChance = 0.0025;
-// const growAmount = 0.0005;
-// const interval = 1.6e-5;
-// const maxSteps = 1000;
-// 
-// let steps = 0;
-// 
-// const s = snowflake();
-// splitEndpoints(s);
-// 
-// let toClear;
-// 
-// function update() {
-//   if (steps < maxSteps) {
-//     if (Math.floor(Math.random() / splitChance) === 0) {
-//       splitEndpoints(s);
-//     }
-//     growEndpoints(s, growAmount);
-//     drawSnowflake(s);
-// 
-//     steps += 1;
-//   } else {
-//     window.clearInterval(toClear);
-//   }
-// }
-// 
-// toClear = window.setInterval(update, interval);
--- snowflake.ts
+++ snowflake.ts
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
+const graphCanvas = document.getElementById('graph') as HTMLCanvasElement;
+const graphCtx = graphCanvas.getContext('2d');
 
 ctx.fillStyle = 'white';
 
@@ -52,39 +54,6 @@ function branchEnd(branch: Branch): Point {
   let x = branch.start.x + l * Math.cos(d);
   let y = branch.start.y + l * Math.sin(d);
   return { x, y };
-}
-
-type GrowthFunction = (time: number) => Growth;
-
-type Growth = { scale: number, growthType: GrowthType };
-
-type GrowthType = 'branching' | 'faceting';
-
-function clamp(x: number, low: number, high: number): number {
-  return Math.min(Math.max(x, low), high);
-}
-
-type NonEmptyArray<T> = { 0: T } & Array<T>;
-
-function test(cond: boolean, name: string): void {
-  if (!cond) {
-    console.error(`Test failure: ${name}`);
-  } else {
-    console.log(`Test success: ${name}`);
-  }
-}
-
-function buildGrowthFunction(growthInput: NonEmptyArray<number>): GrowthFunction {
-  return t => {
-    let s = clamp(t, 0, 1) * growthInput.length;
-    let x = Math.floor(s);
-    let i = x === growthInput.length ? growthInput.length - 1 : x;
-    let signedScale = growthInput[i];
-    return {
-      scale: Math.abs(signedScale),
-      growthType: signedScale > 0.0 ? 'branching' : 'faceting',
-    };
-  };
 }
 
 function drawSnowflake(snowflake: Snowflake): void {
@@ -238,30 +207,80 @@ function addFaceToBranch(snowflake: Snowflake, branch: Branch): void {
   });
 }
 
+
+type NonEmptyArray<T> = { 0: T } & Array<T>;
+type GrowthFunction = (time: number) => Growth;
+type Growth = { scale: number, growthType: GrowthType };
+type GrowthType = 'branching' | 'faceting';
+
+function clamp(x: number, low: number, high: number): number {
+  return Math.min(Math.max(x, low), high);
+}
+
+//function buildGrowthFunction(growthInput: NonEmptyArray<number>): GrowthFunction {
+//  return t => {
+//    let s = clamp(t, 0, 1) * growthInput.length;
+//    let x = Math.floor(s);
+//    let i = x === growthInput.length ? growthInput.length - 1 : x;
+//    let signedScale = growthInput[i];
+//    return {
+//      scale: Math.abs(signedScale),
+//      growthType: signedScale > 0.0 ? 'branching' : 'faceting',
+//    };
+//  };
+//}
+
+let growthInput: NonEmptyArray<number> = [-1, 1, -1];
+
+function interpretGrowth(time: number): Growth {
+  let s = clamp(time, 0, 1) * growthInput.length;
+  let x = Math.floor(s);
+  let i = x === growthInput.length ? growthInput.length - 1 : x;
+  let signedScale = growthInput[i];
+  return {
+    scale: Math.abs(signedScale),
+    growthType: signedScale > 0.0 ? 'branching' : 'faceting',
+  };
+}
+
 const updateInterval = 10;
+const maxSteps = 6000;
 let snowflake = createInitialSnowflake();
 let step = 0;
-let faces = true;
 let intervalId = undefined;
+let currentGrowthType = undefined;
+
+function currentTime(): number {
+  return step / maxSteps;
+}
 
 function update() {
   step += 1;
-  if (faces) {
-    enlargeGrowingFaces(snowflake);
-  } else {
-    enlargeGrowingBranches(snowflake);
+
+  const growth = interpretGrowth(currentTime());
+
+  if (currentGrowthType === undefined) {
+    currentGrowthType = growth.growthType;
   }
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawSnowflake(snowflake);
-  if (step % 700 === 0) {
-    if (faces) {
+
+  if (currentGrowthType !== growth.growthType) {
+    currentGrowthType = growth.growthType;
+    if (currentGrowthType === 'branching') {
       addBranchesToGrowingFaces(snowflake);
     } else {
       addFacesToGrowingBranches(snowflake);
     }
-    faces = !faces;
   }
-  if (step === 6000) {
+
+  if (currentGrowthType === 'branching') {
+    enlargeGrowingBranches(snowflake);
+  } else {
+    enlargeGrowingFaces(snowflake);
+  }
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawSnowflake(snowflake);
+  if (step === maxSteps) {
     window.clearInterval(intervalId);
     console.log('done growing');
   }
@@ -269,19 +288,12 @@ function update() {
 
 intervalId = window.setInterval(update, updateInterval);
 
-function testBuildGrowthFunction(): void {
-  test(buildGrowthFunction([-1, 0.5, 0])(0.0).scale === 1, 'buildGrowthFunction1');
-  test(buildGrowthFunction([-1, 0.5, 0])(0.32).scale === 1, 'buildGrowthFunction2');
-  test(buildGrowthFunction([-1, 0.5, 0])(0.34).scale === 0.5, 'buildGrowthFunction3');
-  test(buildGrowthFunction([-1, 0.5, 0])(0.65).scale === 0.5, 'buildGrowthFunction4');
-  test(buildGrowthFunction([-1, 0.5, 0])(0.67).scale === 0.0, 'buildGrowthFunction5');
-  test(buildGrowthFunction([-1, 0.5, 0])(1).scale === 0.0, 'buildGrowthFunction6');
-  test(buildGrowthFunction([-1, 0.5, 0])(100).scale === 0.0, 'buildGrowthFunction7');
-  test(buildGrowthFunction([-1, 0.5, 0])(-100).scale === 1, 'buildGrowthFunction8');
-
-  test(buildGrowthFunction([-1, 0.5, 0])(0.0).growthType === 'faceting', 'buildGrowthFunction9');
-  test(buildGrowthFunction([-1, 0.5, 0])(0.32).growthType === 'faceting', 'buildGrowthFunction10');
-  test(buildGrowthFunction([-1, 0.5, 0])(0.34).growthType === 'branching', 'buildGrowthFunction11');
+function test(cond: boolean, name: string): void {
+  if (!cond) {
+    console.error(`Test failure: ${name}`);
+  } else {
+    console.log(`Test success: ${name}`);
+  }
 }
 
 function testBranchEnd(): void {
@@ -302,167 +314,4 @@ function testBranchEnd(): void {
   );
 }
 
-testBuildGrowthFunction();
 testBranchEnd();
-
-// const canvas = document.getElementById('canvas') as HTMLCanvasElement ;
-// const ctx = canvas.getContext('2d');
-// 
-// ctx.fillStyle = 'white';
-// 
-// type Angle = number;
-// type Point = { x: number, y: number };
-// 
-// const SPLIT: Angle = Math.PI * 2 / 6;
-// 
-// function point(x: number, y: number): Point {
-//   return {
-//     x: x,
-//     y: y,
-//   }
-// }
-// 
-// type Vertex = {
-//   position: Point,
-//   direction: Angle,
-//   children: Array<Vertex>,
-// };
-// 
-// function vertex(position: Point, direction: Angle): Vertex {
-//   return {
-//     position,
-//     direction,
-//     children: [],
-//   };
-// }
-// 
-// function advanceVertex(vertex: Vertex, amount: number) {
-//   let x = vertex.position.x;
-//   let y = vertex.position.y;
-// 
-//   let xp = x + Math.cos(vertex.direction) * amount;
-//   let yp = y + Math.sin(vertex.direction) * amount;
-// 
-//   let oldDistFromOrigin = Math.sqrt(x * x + y * y);
-//   let newDistFromOrigin = Math.sqrt(xp * xp + yp * yp);
-// 
-//   let diffDist = newDistFromOrigin - oldDistFromOrigin;
-// 
-//   if (diffDist > 0) {
-//     vertex.position.x = xp;
-//     vertex.position.y = yp;
-//   } else {
-//   }
-// }
-// 
-// function snowflake(): Vertex {
-//   return vertex(point(0, 0), 0);
-// }
-// 
-// function isEndpoint(v: Vertex): boolean {
-//   return v.children.length === 0;
-// }
-// 
-// function endpoints(snowflake: Vertex): Array<Vertex> {
-//   const vs = [];
-//   const todo = [snowflake];
-// 
-//   while (todo.length > 0) {
-//     const v = todo.pop();
-//     if (isEndpoint(v)) {
-//       vs.push(v);
-//     } else {
-//       v.children.forEach(c => todo.push(c));
-//     }
-//   }
-// 
-//   return vs;
-// }
-// 
-// function splitEndpoints(snowflake: Vertex) {
-//   const es = endpoints(snowflake);
-//   es.forEach(endpoint => {
-//     let direction = endpoint.direction;
-//     for (let i = 0; i < 6; i += 1) {
-//       endpoint.children.push(
-//         vertex(
-//           point(endpoint.position.x, endpoint.position.y),
-//           direction
-//         ));
-//       direction += SPLIT;
-//     }
-//   });
-// }
-// 
-// function growEndpoints(snowflake: Vertex, amount: number) {
-//   const es = endpoints(snowflake);
-//   es.forEach(endpoint => advanceVertex(endpoint, amount));
-// }
-// 
-// function toCanvasPoint(p: Point): Point {
-//   const result = point(p.x, p.y);
-// 
-//   result.x *= canvas.width;
-//   result.y *= canvas.height;
-// 
-//   result.x += canvas.width * 0.5;
-//   result.y += canvas.height * 0.5;
-// 
-//   return result;
-// }
-// 
-// function drawSnowflake(snowflake: Vertex) {
-//   const todo = [snowflake];
-//   while (todo.length > 0) {
-//     const v = todo.pop();
-//     drawVertex(v);
-//     v.children.forEach(c => todo.push(c));
-//   }
-// }
-// 
-// function drawVertex(v: Vertex) {
-//   ctx.fillStyle = 'white';
-//   ctx.strokeStyle = 'white';
-//   
-//   const p = toCanvasPoint(v.position);
-// 
-//   v.children.forEach(child => {
-//     let c = toCanvasPoint(child.position);
-// 
-//     ctx.beginPath();
-//     ctx.moveTo(p.x, p.y);
-//     ctx.lineTo(c.x, c.y);
-//     ctx.closePath();
-//     ctx.stroke();
-//   });
-// }
-// 
-// ctx.globalAlpha = 0.15;
-// 
-// const splitChance = 0.0025;
-// const growAmount = 0.0005;
-// const interval = 1.6e-5;
-// const maxSteps = 1000;
-// 
-// let steps = 0;
-// 
-// const s = snowflake();
-// splitEndpoints(s);
-// 
-// let toClear;
-// 
-// function update() {
-//   if (steps < maxSteps) {
-//     if (Math.floor(Math.random() / splitChance) === 0) {
-//       splitEndpoints(s);
-//     }
-//     growEndpoints(s, growAmount);
-//     drawSnowflake(s);
-// 
-//     steps += 1;
-//   } else {
-//     window.clearInterval(toClear);
-//   }
-// }
-// 
-// toClear = window.setInterval(update, interval);
