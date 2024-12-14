237051a1942c13586e3c0e68b450948f2a7340fb
Michael MacLeod <michaelmmacleod@gmail.com>
2024-12-14 00:29:45 -0800

    Delete unused code

--- src/Side.ts
+++ src/Side.ts
@@ -24,41 +24,6 @@ export function zero(): Side {
   }
 }
 
-// Returns a Side calculated by rotating a 'Side2d' around the origin
-// counterclockwise until it is horizontal. 'absoluteDirection' should be the
-// non-relative number of the side, starting from the rightmost upward
-// side and going counterclockwise. 'absoluteDirection' is zero indexed.
-//
-// absoluteDirection = ...
-//
-//         1
-//       -----
-//    2 /     \  0
-//     /       \
-//     \       /
-//   3  \     /  5
-//       -----
-//         4
-export function normalizeSide2D(side2d: Side2D, absoluteDirection: number): Side {
-  const theta = oneSixthCircle * (1 - absoluteDirection);
-  const left = Points.rotate(side2d.left, theta);
-  const right = Points.rotate(side2d.right, theta);
-  return {
-    left: left.x,
-    right: right.x,
-    height: left.y,
-  };
-}
-
-export function normalizeSide2DM(result: Side, side2d: Side2D, absoluteDirection: number): void {
-  const theta = oneSixthCircle * (1 - absoluteDirection);
-  const left = Points.rotate(side2d.left, theta);
-  const right = Points.rotate(side2d.right, theta);
-  result.left = left.x;
-  result.right = right.x;
-  result.height = left.y;
-}
-
 export function normalizeSide2DFaceM(result: Side, face: Face, absoluteDirection: number): void {
   const theta = oneSixthCircle * (1 - absoluteDirection);
   const side2dLeftX = Side2Ds.faceSideNLeftX(face, absoluteDirection);
@@ -85,17 +50,6 @@ export function normalizeSide2DBranchM(result: Side, branch: Branch, absoluteDir
   result.height = left.y;
 }
 
-export function normalizeRelativeSide2Ds(side2Ds: Array6<Side2D>, shapeDir: Direction): Array6<Side> {
-  return mapArray6(side2Ds, (s, i) => normalizeSide2D(s, (i + shapeDir) % Directions.values.length));
-}
-
-export function normalizeRelativeSide2DsM(result: Array6<Array<Side>>, partIndex: number, side2Ds: Array6<Side2D>, shapeDir: Direction): void {
-  for (let i = 0; i < Directions.values.length; ++i) {
-    const relativeDirection = rem((i - shapeDir), Directions.values.length);
-    normalizeSide2DM(result[i][partIndex], side2Ds[relativeDirection], i);
-  }
-}
-
 export function normalizeFaceRelativeSide2DsM(result: Array6<Array<Side>>, partIndex: number, face: Face): void {
   for (let i = 0; i < Directions.values.length; ++i) {
     normalizeSide2DFaceM(result[i][partIndex], face, i);
@@ -106,28 +60,6 @@ export function normalizeBranchRelativeSide2DsM(result: Array6<Array<Side>>, par
   for (let i = 0; i < Directions.values.length; ++i) {
     normalizeSide2DBranchM(result[i][partIndex], branch, i);
   }
-}
-
-export function normalizeFaceSidesM(result: Array6<Array<Side>>, faceIndex: number, face: Face): void {
-  normalizeRelativeSide2DsM(result, faceIndex, Side2Ds.ofFace(face), face.direction);
-}
-
-// Normalizes the sides of a face. Sides are returned in relative order to their
-// un-normalized counterparts.
-//                  1
-//       -------------------------
-//    2 /                         \  0
-//     /                           \ _____direction___>
-//     \                           /
-//   3  \                         /  5
-//       -------------------------
-//                  4
-export function normalizedBranchSides(branch: Branch): Array<Side> {
-  return normalizeRelativeSide2Ds(Side2Ds.ofBranch(branch), branch.direction);
-}
-
-export function normalizeBranchSidesM(result: Array6<Array<Side>>, branchIndex: number, branch: Branch): void {
-  normalizeRelativeSide2DsM(result, branchIndex, Side2Ds.ofBranch(branch), branch.direction);
 }
 
 // Returns how far above s1 is from s2 if s1 is above and overlapping
--- test/Main.ts
+++ test/Main.ts
@@ -11,17 +11,6 @@ function sum(a: number, b: number): number {
     return Points.add(p1, p2).x;
 }
 
-test('normalize centered', () => {
-    const f = Faces.zero();
-    f.size = 1;
-    const sides = Side2Ds.ofFace(f).map((s, i) => Sides.normalizeSide2D(s, i));
-    sides.forEach(s => {
-        expect(s.left).toBeCloseTo(-0.5);
-        expect(s.right).toBeCloseTo(0.5);
-        expect(s.height).toBeCloseTo(0.866);
-    });
-});
-
 // test('normalize offset', () => {
 //     const f = Faces.zero();
 //     f.size = 1;
