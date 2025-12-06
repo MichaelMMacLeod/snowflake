65c725dfadcffc3aaa8e2603e73e882bd43c9a93
Michael MacLeod <michaelmmacleod@gmail.com>
2025-12-05 19:52:22 -0800

    Revert "Fix bugs from refactor"
    This reverts commit e0b7fb3ca6bc6bcdbf819393777eecf088147097.

--- dist/snowflake/Branch.d.ts
+++ dist/snowflake/Branch.d.ts
@@ -4,4 +4,4 @@ export declare const endCenterX: (startX: number, length: number, d: Direction) 
 export declare const endCenterY: (startY: number, length: number, d: Direction) => number;
 export declare const pointNX: (startX: number, length: number, branchDirection: Direction, size: number, absoluteDirection: number) => number;
 export declare const pointNY: (startY: number, length: number, branchDirection: Direction, size: number, absoluteDirection: number) => number;
-export declare const draw: (g: Graphic, startX: number, startY: number, size: number, length: number, d: Direction) => boolean;
+export declare const draw: (g: Graphic, startX: number, startY: number, d: Direction, size: number) => boolean;
--- src/snowflake/SnowflakeObject.ts
+++ src/snowflake/SnowflakeObject.ts
@@ -219,15 +219,14 @@ export const draw = (g: Graphic, snowflake: Snowflake, foregroundColor: string):
     const size = snowflake[_face_sizes][fi];
     const d = snowflake[_face_directions][fi] as Direction;
     const isFirstFace = fi === 0;
-    anyPartOutside ||= Faces.draw(g, centerX, centerY, size, d, isFirstFace);
+    return anyPartOutside ||= Faces.draw(g, centerX, centerY, size, d, isFirstFace)
   });
   forEachGrowingBranch(snowflake, bi => {
     const startX = snowflake[_branch_start_xs][bi];
     const startY = snowflake[_branch_start_ys][bi];
     const d = snowflake[_branch_directions][bi] as Direction;
     const size = snowflake[_branch_sizes][bi];
-    const length = snowflake[_branch_lengths][bi];
-    anyPartOutside ||= Branches.draw(g, startX, startY, size, length, d);
+    anyPartOutside ||= Branches.draw(g, startX, startY, d, size)
   });
   g[_graphic_ctx].stroke();
   return anyPartOutside;
